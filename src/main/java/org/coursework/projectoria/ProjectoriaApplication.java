c5c22d8f2bc2e908fedad72c4a9800badc7c4756
sobol <215797@fa.ru>
2023-03-29 15:42:29 +0300

    start new project

---
+++ src/main/java/org/coursework/projectoria/ProjectoriaApplication.java
@@ -0,0 +1,13 @@
+package org.coursework.projectoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectoriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coursework/projectoria/ProjectoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coursework.projectoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
